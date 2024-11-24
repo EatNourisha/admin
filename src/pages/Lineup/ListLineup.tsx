de634b4c4476b9854ed9953427ba07091a3ecf31
nnodim <nnodimobinna@gmail.com>
2024-11-24 10:28:26 +0100

    added date filter to lineups

--- src/pages/Lineup/ListLineup.tsx
+++ src/pages/Lineup/ListLineup.tsx
@@ -96,7 +96,7 @@ export default function ListLineup() {
         queryString +=
           filters.sortBy === "createdAt"
             ? ""
-            : `&order=-1&sortby=${filters.sortBy}`;
+            : `&order=1&sortby=${filters.sortBy}`;
       }
 
       if (filters.week && filters.week !== "all") {
